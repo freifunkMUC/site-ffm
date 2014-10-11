6a65590ffd608d8f1e47f05652a8ec01fc1fec7e
Ole <amperprirat@gmx.de>
2014-10-11 18:45:37 +0200

    updated site.conf for more information see site.conf

--- site.mk
+++ site.mk
@@ -22,7 +22,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6~stable$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
