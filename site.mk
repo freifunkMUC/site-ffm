11e58e4d0caeb3ca06ed335c05783436da261881
Franz Pletz <fpletz@fnordicwalking.de>
2015-10-04 13:10:15 +0200

    Reenable ebtables filters

--- site.mk
+++ site.mk
@@ -9,7 +9,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
-  gluon-config-mode-site-select \
+	gluon-config-mode-site-select \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
@@ -24,9 +24,9 @@ GLUON_SITE_PACKAGES := \
 	gluon-status-page \
 	iwinfo \
 	iptables \
-	haveged #\
-#	gluon-ebtables-filter-arp-ffmuc \
-#	gluon-ebtables-filter-multicast-ffmuc
+	haveged \
+	gluon-ebtables-filter-arp-ffmuc \
+	gluon-ebtables-filter-multicast-ffmuc
 
 
 DEFAULT_GLUON_RELEASE := snapshot~$(shell date '+%Y%m%d')
