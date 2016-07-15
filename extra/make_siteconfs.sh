46d5fa4877350905786fa0a4c7111ba1a10f4fc1
Markus Rippl <m-kontakt@rippl.name>
2016-07-16 00:33:36 +0200

    change also site_name and site_code

--- extra/make_siteconfs.sh
+++ extra/make_siteconfs.sh
@@ -5,6 +5,6 @@
 cat stadtteile_idx.txt|grep -e '^[^#]'|grep -v LK_|while IFS=, read nr id desc port; do cp ffmuc.conf new/ffmuc_$id.conf; done
 cat stadtteile_idx.txt|grep -e '^[^#]'|grep  LK_|while IFS=, read nr id desc port; do cp ffmuc_umland.conf new/ffmuc_$id.conf; done
 
+cat stadtteile_idx.txt|grep -e '^[^#]'|while IFS=, read nr id desc seg_id port; do ( echo "# processing ffmuc_$id.conf"; cat new/ffmuc_$id.conf |sed -e "s/port 100[01][0-9]/port $port/" |sed -e "s/.*site_code.*/  site_code = 'ffmuc_$id',/g"|sed -e "s/.*site_name.*/  site_name = 'Freifunk München - $desc',/g" > new/ffmuc_$id.conf.new; mv new/ffmuc_$id.conf.new new/ffmuc_$id.conf); done
 
-cat stadtteile_idx.txt|grep -e '^[^#]'|while IFS=, read nr id desc seg_id port; do ( echo "# processing ffmuc_$id.conf"; cat new/ffmuc_$id.conf |sed -e "s/port 100[01][0-9]/port $port/" > new/ffmuc_$id.conf.new; mv new/ffmuc_$id.conf.new new/ffmuc_$id.conf); done
 
