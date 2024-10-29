2e6d6aecdce998490c4abec2cc0726787f790825
Tobias <5702338+T0biii@users.noreply.github.com>
2024-10-29 22:34:11 +0100

    site: remove *GLUON_RELEASE (#505)
    (cherry picked from commit a55d9d35709278e080698e35da89cc46f7e5009a)

--- site.mk
+++ site.mk
@@ -1,11 +1,6 @@
 GLUON_MULTIDOMAIN=1
 GLUON_DEPRECATED=upgrade
 
-DEFAULT_GLUON_RELEASE := v2021.6.0~exp$(shell date '+%Y%m%d%H')
-
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
 GLUON_PRIORITY ?= 0
 
 GLUON_REGION ?= eu
