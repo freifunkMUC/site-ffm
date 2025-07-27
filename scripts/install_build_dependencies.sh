080722de5a5ae1ba0fdf159f7e28a747ba8c9797
Grische <2787581+grische@users.noreply.github.com>
2025-07-27 21:49:06 +0200

    scripts: add bpftool dependency for ffmuc-ebpf-clat

--- scripts/install_build_dependencies.sh
+++ scripts/install_build_dependencies.sh
@@ -39,3 +39,14 @@ apt-get -y --no-install-recommends install \
     tar \
     unzip \
     wget
+
+# We require bpftool 7.5+
+# https://github.com/libbpf/bpftool/releases#:~:text=work%20with%20object%20files%20in%20either%20endianness%20for%20some%20operations%20like%20object%20linking%20or%20light%20BPF%20skeleton%20creation
+apt-get -y --no-install-recommends install bpftool || # Ubuntu 25.04+
+{
+    # Ubuntu 24.04 do not have bpftool as a package and rely on "linux-tools-$(uname -r)", but those can deliver only bpftool 7.4
+    curl -LO https://github.com/libbpf/bpftool/releases/download/v7.6.0/bpftool-v7.6.0-amd64.tar.gz
+    curl -LO https://github.com/libbpf/bpftool/releases/download/v7.6.0/bpftool-v7.6.0-amd64.tar.gz.sha256sum
+    tar xvf bpftool-v7.6.0-amd64.tar.gz -C /usr/local/bin
+    chmod +x /usr/local/bin/bpftool
+}
