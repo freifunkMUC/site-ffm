990a810e54e49c7d57b0e349f2c7bb3c6c2b87d2
Grische <2787581+grische@users.noreply.github.com>
2025-07-27 21:49:06 +0200

    scripts: add bpftool dependency for ffmuc-ebpf-clat

--- scripts/install_build_dependencies.sh
+++ scripts/install_build_dependencies.sh
@@ -39,3 +39,14 @@ apt-get -y --no-install-recommends install \
     tar \
     unzip \
     wget
+
+# Install bpftool
+apt-get -y --no-install-recommends install bpftool || # Ubuntu 25.04+
+{
+    # Fallback for Ubuntu 24.04 and earlier
+    latest_linux_tools=$(apt -q search "^linux-tools-[0-9].*-generic$" | grep "^linux-tools" | awk -F '/' '{ print $1 }' | sort -V | tail -n 1)
+    apt-get -y --no-install-recommends install "${latest_linux_tools}"
+    latest_bpftool=$(find /usr/lib/linux-tools/ -name bpftool | sort -V | tail -n 1)
+    rm -f /usr/sbin/bpftool # delete wrapper bpftool script
+    ln -s "${latest_bpftool}" /usr/sbin/bpftool
+}
