06d0c86ce915ee563d72eeaddba0221512914dc7
Grische <github@grische.xyz>
2026-08-24 15:49:13 +0200

    temporary disable node-whisperer until we have time to fix it

--- image-customization.lua
+++ image-customization.lua
@@ -23,7 +23,7 @@ packages{
     'ffac-change-autoupdater',
     'ffac-ssid-changer',
     'ffbs-collect-debug-info',
-    'ffda-node-whisperer',
+--    'ffda-node-whisperer',
     'ffmuc-ipv6-ra-filter',
     'ffmuc-mesh-vpn-wireguard-vxlan',
     'iwinfo',
